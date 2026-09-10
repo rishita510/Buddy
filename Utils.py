8a3f8ee64e1877ccd96150e3917753c8631624e8
rishita510 <isroscience2@gmail.com>
2026-09-10 22:56:31 +0530

    added regular expression

--- Utils.py
+++ Utils.py
@@ -4,7 +4,6 @@ _PATTERNS = [
     r"(?:youtube\.com/watch\?v=|youtube\.com/embed/|youtu\.be/)([a-zA-Z0-9_-]{11})",
 ]
 
-
 def extract_video_id(url: str) -> str:
     """Pulls the 11-character video ID out of any common YouTube URL shape."""
     for pattern in _PATTERNS:
